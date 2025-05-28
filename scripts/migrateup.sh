3ca23e6700dc142412b77ce0db65c30bd582ee8b
mambo-dev <mambo.michael.22@gmail.com>
2025-05-28 03:43:09 +0300

    fix auth token

--- scripts/migrateup.sh
+++ scripts/migrateup.sh
@@ -5,7 +5,7 @@ if [ -f .env ]; then
 fi
 
 # Append authToken to the DATABASE_URL
-FULL_DATABASE_URL="${DATABASE_URL}?authToken=${TAUTHTOKEN}"
+FULL_DATABASE_URL="${DATABASE_URL}?authToken=${TAUTH_TOKEN}"
 
 cd sql/schema
 goose turso "$FULL_DATABASE_URL" up
